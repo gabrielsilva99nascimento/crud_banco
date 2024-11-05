6b331fa98f193c78a5afc47abd467ee5ccad4a50
Gabriel Silva <gabrielsilvagns99@gmail.com>
2024-11-05 18:01:27 -0300

    fix transaction component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { TransactionComponent } from '../components/transaction/transacaoComponent';
+import { TransactionComponent } from '../components/transaction/transactionComponent';
 
 @NgModule({
   declarations: [
